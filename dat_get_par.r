f1bd33ce1d051107217e7065770d9c3bb3f0fb5a
Xiao Xiao <xiao@weecology.org>
2014-04-16 21:32:34 -0600

    Remove dat_get_par.r. Code is now in run_analyses.py.

--- dat_get_par.r
+++
@@ -1,8 +0,0 @@
-source('dist_par_est.r')
-
-dat_list = c('FERP', 'ACA', 'WesternGhats', 'BCI', 'BVSF', 'Cocoli', 'Lahei', 
-             'LaSelva', 'Luquillo', 'NC', 'Oosting', 'Serimbu', 'Shirakami', 'Sherman')
-             
-get_par(dat_list, 'par_est.csv')
-
-
